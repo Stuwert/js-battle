b44e3dfbcd01a22998d52634551e896925226f22
Stuart Urback <stuart@urback.net>
2015-12-05 18:54:36 -0700

    Greater than fxn complete

--- js/game.js
+++ js/game.js
@@ -1,7 +1,9 @@
 module.exports = {
 
 
-  
+  greaterThan: function(char1Attack, char2Attack){
+   return char1Attack > char2Attack ? char1Attack : char2Attack;
+  }
 
 
 }
--- spec/game_spec.js
+++ spec/game_spec.js
@@ -1,8 +1,15 @@
 var myFunction = require('../js/game');
 
 
-describe('first desrcription', function(){
-  it('returns something about the description', function(){
-    expect(myFunction.main()).toEqual();
+describe('Compares two attacks', function(){
+  it('returns the greater attack', function(){
+    var char1 = 10, char2 = 12;
+    expect(myFunction.greaterThan(char1, char2)).toEqual(char2);
   })
+  it('returns the greater attack', function(){
+    var char1 = -1, char2 = 1;
+    expect(myFunction.greaterThan(char1, char2)).toEqual(char2);
+  })
+
+
 })

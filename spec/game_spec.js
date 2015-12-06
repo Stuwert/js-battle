74fbf1e8c82e4c46441b91407876ca1e671a57bb
Stuart Urback <stuart@urback.net>
2015-12-05 18:42:45 -0700

    Init tests

---
+++ spec/game_spec.js
@@ -0,0 +1,8 @@
+var myFunction = require('../js/game');
+
+
+describe('first desrcription', function(){
+  it('returns something about the description', function(){
+    expect(myFunction.main()).toEqual();
+  })
+})
